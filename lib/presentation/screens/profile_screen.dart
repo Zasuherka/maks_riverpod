ed017700169a4969d95e090f3239e38afb89cc03
Maks <zasuherok2002@gmail.com>
2026-06-14 13:06:12 +0700

    some fixes

--- lib/presentation/screens/profile_screen.dart
+++ lib/presentation/screens/profile_screen.dart
@@ -10,7 +10,6 @@ class ProfileScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final auth = ref.watch(authProvider);
-    print('object');
 
     return Scaffold(
       appBar: AppBar(title: const Text('Профиль')),
